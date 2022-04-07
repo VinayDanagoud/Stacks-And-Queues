0f149b71f56333f6b4df8a0b9006571442c9c613
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-04-07 16:50:39 +0530

    Stacks And Queues

---
+++ Stacks And Queues/Stacks And Queues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pushing values to Stack");
+            LinkedListStack mystack = new LinkedListStack();
+            mystack.Push(70);
+            mystack.Push(30);
+            mystack.Push(56);
+            mystack.Display();
+
+        }
+    }
+}
