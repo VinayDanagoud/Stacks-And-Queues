1732d25d89f1e4c2f7dd38979ed8af649f0cc663
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-04-07 18:33:26 +0530

    Remove The Elements Of Queues

--- Stacks And Queues/Stacks And Queues/Program.cs
+++ Stacks And Queues/Stacks And Queues/Program.cs
@@ -20,12 +20,12 @@ namespace StacksAndQueues
 
 
 
-
-
             LinkedListQueue queue = new LinkedListQueue();
             queue.Enqueue(56);
             queue.Enqueue(30);
             queue.Enqueue(70);
+            queue.Display();
+            queue.Dequeue();
             queue.Display();
         }
     }
